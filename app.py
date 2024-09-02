57d2508b2e57083096e02284c6a92672070d4616
andre178 <andre.chepetan@gmail.com>
2024-09-02 15:01:11 -0700

    replaced it all

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
